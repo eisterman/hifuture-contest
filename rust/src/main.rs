41a188a745bfd8da45c5fdabfc0a0093de0bd717
Federico Pasqua <federico.pasqua.96@gmail.com>
2018-05-31 17:19:00 +0200

    Fix logical errors in Rust version, plus gitignore

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,19 +2,20 @@ use std::io;
 use std::process::exit;
 
 fn main() {
-    print!("Inserisci un numero e ti dirò se ci piace: ");
-    let buffer = String::new();
-    io::stdin().read_line(&mut buffer);
-    if !buffer.chars().all(|x| x.is_ascii_digit()) || buffer.is_empty() {
+    println!("Inserisci un numero e ti dirò se ci piace: ");
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let data = buffer.trim();
+    if !data.chars().all(|x| x.is_ascii_digit()) || data.is_empty() {
         eprintln!("Input non numerico!");
         exit(1);
     }
-    if buffer.len() < 2 {
+    if data.len() < 2 {
         eprintln!("Il numero deve essere lungo almeno 2 cifre!");
         exit(1)
     }
-    for i in 0..(len(buffer)-1) {
-        if abs(buffer[i] as i64 - buffer[i+1] as i64) != 1 {
+    for i in 0..(data.len()-1) {
+        if (data.chars().nth(i).unwrap() as i64 - data.chars().nth(i+1).unwrap() as i64).abs() != 1 {
             println!("Il numero non ci piace :c");
             exit(0)
         }
